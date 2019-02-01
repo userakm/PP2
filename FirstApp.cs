f52b09ef651c9fa9513e22bad9e54b9eafdac9e0
userakm <kalbaeva7@gmail.com>
2019-02-01 17:44:37 +0600

    Tasks

---
+++ FirstApp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculator.Add(23, 15);
+            Console.ReadKey();
+        }
+    }
+    class Calculator
+    {
+        public static void Add(int a, int b)
+        {
+            int x = a + b;
+            Console.WriteLine($"addition of {a} and {b} is equal to {x}");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());//create integer - number of lines
+
+            for(int i=0; i<n; i++)//amount of lines
+            {
+                for(int j=1; j<=i+1; j++)//number of columns
+                {
+                    Console.Write("[*]");//this command helps us to write [*]
+                }
+                Console.WriteLine();//it switches to the next line
+            }
+            Console.ReadLine();//terminal won't be closed, until you won't press any key to do so 
+        }
+    }
+}
